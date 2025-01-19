9f365005812e3895d9a8b2e80bb13281beb5aba9
윤다빈 <czmcm5@gmail.com>
2025-01-19 20:44:40 +0900

    1/19(일) 커스텀 useCallback 구현

--- src/@lib/hooks/useCallback.ts
+++ src/@lib/hooks/useCallback.ts
@@ -1,10 +1,13 @@
-/* eslint-disable @typescript-eslint/no-unused-vars,@typescript-eslint/no-unsafe-function-type */
-import { DependencyList } from "react";
+/* eslint-disable @typescript-eslint/no-unsafe-function-type */
+import { DependencyList, useMemo } from "react";
 
 export function useCallback<T extends Function>(
   factory: T,
   _deps: DependencyList,
 ) {
-  // 직접 작성한 useMemo를 통해서 만들어보세요.
-  return factory as T;
+  // useMemo가 의존성 배열에 따라 기존의 것, 새로 생성된 것을 던져줌.
+  // useCallback(fn, _deps) 와 useMemo(()=>fn,_deps)는 같다
+  // 그러면 useCallback 왜쓰지..?
+  // 구분하는 이유: 가독성과 의도 명시
+  return useMemo(() => factory, _deps);
 }
