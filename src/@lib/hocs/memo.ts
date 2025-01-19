e2fed1593f7bc49e3d2886bd02728baf330a0ee3
윤다빈 <czmcm5@gmail.com>
2025-01-19 22:56:35 +0900

    1/19(일) 커스텀 React.memo 구현

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,22 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import React, { ComponentType, useRef } from "react";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
-  _equals = shallowEquals,
+  _equals = shallowEquals
 ) {
-  return Component;
+  const MemoComponent = (props: P) => {
+    const preProps = useRef<P | null>(null);
+    const preRender = useRef<JSX.Element | null>(null);
+
+    // props 변화 감지
+    if (!preProps.current || !_equals(preProps.current, props)) {
+      preProps.current = props;
+      preRender.current = React.createElement(Component, props); //  preRender.current = <Component {...props} />;
+    }
+    // props 변화 없음
+    return preRender.current;
+  };
+  return MemoComponent;
 }
